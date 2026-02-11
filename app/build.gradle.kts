87ba2d42856dadeb6f4f01a3aa778ae22954b80d
nhirokinet <nhirokinet@nhiroki.net>
2026-02-12 00:32:57 +0900

    remove material library which was from initializating project and use different theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,6 @@ android {
 
 dependencies {
     implementation(libs.appcompat)
-    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
