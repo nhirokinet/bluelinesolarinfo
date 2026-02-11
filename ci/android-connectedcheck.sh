aa4331ed611c0e4beb0a751a55bd97d144f7905c
nhirokinet <nhirokinet@nhiroki.net>
2026-02-11 15:31:06 +0900

    add connectedCheck to CI

---
+++ ci/android-connectedcheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+# Minimum supported version: SDK31
+
+for sdkver in 36 31
+do
+    sdkmanager platform-tools emulator "system-images;android-$sdkver;default;x86_64"
+    echo no | avdmanager create avd -f -n vm$sdkver -k "system-images;android-$sdkver;default;x86_64"
+    ${ANDROID_HOME}/emulator/emulator -avd vm$sdkver -no-window -no-audio &
+    EMULATOR_PID=$!
+    for i in $(seq 60); do if adb -e shell getprop init.svc.bootanim ; then break ; fi; sleep 5; done
+    adb -e shell getprop init.svc.bootanim
+    for i in $(seq 60); do if adb -e shell getprop init.svc.bootanim | grep stopped ; then break ; fi; sleep 5; done
+    adb -e shell getprop init.svc.bootanim | grep stopped
+    for i in $(seq 60); do if adb -e shell pm list packages ; then break ; fi; sleep 5; done
+    adb -e shell pm list packages
+    for i in $(seq 10); do if adb -e shell input keyevent 82 ; then break; fi; sleep 5; done  # unlock device
+    for i in $(seq 24); do if adb -e shell dumpsys power | grep mHoldingWakeLockSuspendBlocker=true ; then break; fi; sleep 5; done
+    sleep 30
+    ./gradlew connectedCheck || (sleep 30 && ./gradlew connectedCheck)
+    kill $EMULATOR_PID
+    while test -e /proc/$EMULATOR_PID; do sleep 1; done
+    avdmanager delete avd -n vm$sdkver
+done
