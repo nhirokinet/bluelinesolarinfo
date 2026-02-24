781f8aa017d13ecf4f63f0479fdc6eea4a8eb514
nhirokinet <nhirokinet@nhiroki.net>
2026-02-24 23:39:01 +0900

    fix tokyo location

--- app/src/test/java/net/nhiroki/lib/bluelineastrolib/test_data/locationsForTest/TokyoNAO.java
+++ app/src/test/java/net/nhiroki/lib/bluelineastrolib/test_data/locationsForTest/TokyoNAO.java
@@ -6,6 +6,6 @@ import net.nhiroki.lib.bluelineastrolib.location.LocationOnTheEarth;
 public class TokyoNAO extends LocationOnTheEarth {
     public TokyoNAO() {
         // https://eco.mtk.nao.ac.jp/koyomi/dni/2024/s1301.html
-        super(139.7447, 35.6581, 0.0);
+        super(139.7414, 35.6581, 0.0);
     }
 }
