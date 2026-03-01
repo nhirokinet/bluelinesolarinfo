92e0209ee4471b29e3ac7d6eab7e3418c497a6e2
nhirokinet <nhirokinet@nhiroki.net>
2026-03-01 23:58:43 +0900

    add and improve tests (#12)

---
+++ app/src/androidTest/java/net/nhiroki/bluelinesolarinfo/test_data/NorthPoleLocation.java
@@ -0,0 +1,9 @@
+package net.nhiroki.bluelinesolarinfo.test_data;
+
+import net.nhiroki.lib.bluelineastrolib.location.LocationOnTheEarth;
+
+public class NorthPoleLocation extends LocationOnTheEarth {
+    public NorthPoleLocation() {
+        super(0.0, 90.0, 0.0);
+    }
+}
---
+++ app/src/androidTest/java/net/nhiroki/bluelinesolarinfo/test_data/NorthPoleRegion.java
@@ -0,0 +1,11 @@
+package net.nhiroki.bluelinesolarinfo.test_data;
+
+import net.nhiroki.bluelinesolarinfo.region.RegionOnTheEarth;
+
+import java.time.ZoneId;
+
+public class NorthPoleRegion extends RegionOnTheEarth {
+    public NorthPoleRegion() {
+        super(0, "North Pole", ZoneId.of("UTC"), new NorthPoleLocation());
+    }
+}
