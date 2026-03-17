2ea0eb33e8949aa8557a88fff93778e737d41490
nhirokinet <nhirokinet@nhiroki.net>
2026-03-17 21:54:52 +0900

    Bump version to 0.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "net.nhiroki.bluelinesolarinfo"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
